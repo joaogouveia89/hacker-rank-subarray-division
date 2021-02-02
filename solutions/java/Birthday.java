471a0503209cbf86b16c1d16b6fb5b0627b5b214
Joao Gouveia <joao.gouveia@criticaltechworks.com>
2021-02-02 07:44:06 +0000

    java solution done

--- solutions/java/Birthday.java
+++ solutions/java/Birthday.java
@@ -1,5 +1,20 @@
 public class Birthday{
 	public static int run(int[] s, int d, int m){
-		return 0;
+		int ways = 0;
+		int sum = 0;
+		int arraySize = s.length;
+
+		for(int i = 0; i < arraySize ; i++){
+			if(i + m > arraySize) break;
+
+			for(int j = i; j < (i + m); j++){
+				sum += s[j];
+			}
+
+			if(sum == d) ways++;
+
+			sum = 0;
+		}
+		return ways;
 	}
 }
